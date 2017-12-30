68e09486d266ddf23f31a23f0f6f6ec07791b792
Hugo Linsenmaier <hugo.linsenmaier@epita.fr>
2017-12-30 15:31:07 +0100

    [MODIFY] debug.c: Split program attach and prompt into different functions

--- src/debug.c
+++ src/debug.c
@@ -1,33 +1,61 @@
-#include <err.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/ptrace.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
+#include <string.h>
+#include <readline/history.h>
+#include <readline/readline.h>
+#include <errno.h>
 
+#include <sys/ptrace.h>
+#include <sys/wait.h>
+
+#include "commands.h"
 #include "debug.h"
 
-int launch_debug(char *argv[])
+struct g_program g_program;
+
+int attach_program(char *argv[])
+{
+    if (ptrace(PTRACE_TRACEME, 0, 0, 0) < 0) {
+        perror("");
+        return 0;
+    }
+    execvp(argv[1], argv + 1);
+    exit(EXIT_FAILURE);
+}
+
+int handle_debug()
+{
+    int status;
+    if (wait(&status) < 0) {
+        perror("");
+        return 0;
+    }
+
+    while (1) {
+        char *cmd = readline("dbg> ");
+        if (!strcmp(cmd, "quit")) {
+            free(cmd);
+            return 1;
+        }
+        dispatch_command(cmd);
+        free(cmd);
+    }
+}
+
+int debug(char *argv[])
 {
     pid_t pid = fork();
-
-    if (pid < 0)
-        err(1, "Call to fork failed");
+    if (pid < 0) {
+        perror("");
+        return 0;
+    }
     else if (!pid)
     {
-        ptrace(PTRACE_TRACEME, 0, 0, 0);
-        execvp(argv[1], argv + 1);
-        exit(EXIT_FAILURE);
+        return attach_program(argv);
     }
     else
     {
-        int status;
-        if (waitpid(pid, &status, 0) < 0)
-            return 0;
-
-        if (WIFEXITED(status))
-            return 1;
+        g_program.pid = pid;
+        return handle_debug();
     }
-    return 0;
 }
--- src/includes/debug.h
+++ src/includes/debug.h
@@ -1,6 +1,17 @@
 #ifndef DEBUG_H
 #define DEBUG_H
 
-int launch_debug(char *argv[]);
+#include <unistd.h>
+
+struct g_program
+{
+    pid_t pid;
+};
+
+int attach_program(char *argv[]);
+
+int handle_debug();
+
+int debug(char *argv[]);
 
 #endif /* !DEBUG_H */
