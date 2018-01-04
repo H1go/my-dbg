a2a3164ebfb03147ab592750dc32115f3b04b734
Hugo Linsenmaier <hugo.linsenmaier@epita.fr>
2018-01-04 01:41:25 +0100

    [MODIFY] breakpoint.c: Add error checking and fix implementation

--- src/breakpoint.c
+++ src/breakpoint.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/ptrace.h>
@@ -24,22 +25,32 @@ void breakpoint_activate(struct breakpoint *b)
 {
     b->activated = 1;
     long data = ptrace(PTRACE_PEEKDATA, g_program.pid, (void *)b->addr, 0);
+    if (data < 0 && errno)
+        perror("Activate 1");
+
     b->data = (data & 0xFF);
-    printf("Original data at 0x%lx: 0x%lx", b->addr, b->data);
+    printf("Original data at 0x%lx: 0x%lx\n", b->addr, b->data);
 
     long int3 = ((data & ~0xFF) | 0xCC);
     data = ptrace(PTRACE_POKEDATA, g_program.pid, (void *)b->addr, int3);
+    if (data < 0 && errno)
+        perror("Activate 2");
 
-    printf("Data at 0x%lx is now: 0x%lx", b->addr, data);
+    printf("Data at 0x%lx is now: 0x%lx\n", b->addr, data);
 }
 
 void breakpoint_deactivate(struct breakpoint *b)
 {
     long data = ptrace(PTRACE_PEEKDATA, g_program.pid, (void *)b->addr, 0);
+    if (data < 0 && errno)
+        perror("");
+
     assert((data & 0xFF) == 0xCC);
 
     long save = ((data & ~0xFF) | b->data);
-    ptrace(PTRACE_POKEDATA, g_program.pid, (void *)b->addr, save);
+    if (ptrace(PTRACE_POKEDATA, g_program.pid, (void *)b->addr, save) < 0)
+        if (errno)
+            perror("");
     b->activated = 0;
 }
 
@@ -49,6 +60,8 @@ void breakpoint_set_at_adress(char *addr)
     if (!b)
         return;
 
-    printf("Set breakpoint at adress 0x%lx", b->addr);
+    printf("Set breakpoint at adress 0x%lx\n", b->addr);
     breakpoint_activate(b);
+
+    list_push(g_program.breakpoints, b);
 }
