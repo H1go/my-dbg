08050257687b1526969050aaa78d02298d03441e
Hugo Linsenmaier <hugo.linsenmaier@epita.fr>
2018-01-04 04:36:53 +0100

    [ADD] commands_print.c: Split commands.c in another file

--- src/commands_print.c
+++ src/commands_print.c
@@ -1,4 +1,28 @@
-//
-// Created by hugo on 04/01/18.
-//
+#include <stdio.h>
 
+#include "commands.h"
+#include "debug.h"
+#include "register.h"
+
+struct g_program g_program;
+
+int break_list(void *arg)
+{
+    arg = arg;
+    list_print(g_program.breakpoints);
+    return 1;
+}
+
+int info_regs(void *arg)
+{
+    arg = arg;
+    register_print();
+    return 1;
+}
+
+int quit(void *arg)
+{
+    arg = arg;
+    printf("Quit\n");
+    return 1;
+}
