359b1c98dc1f586a381aa2c31b796f8c7b80e573
Hugo Linsenmaier <hugo.linsenmaier@epita.fr>
2018-01-04 04:39:47 +0100

    [ADD] continue.c: Implement continue command

--- src/continue.c
+++ src/continue.c
@@ -1,4 +1,18 @@
-//
-// Created by hugo on 04/01/18.
-//
+#include <stdio.h>
+#include <sys/ptrace.h>
 
+#include "debug.h"
+#include "sig.h"
+
+struct g_program g_program;
+
+int continue_execution(void *arg)
+{
+    arg = arg;
+    restore_context();
+    if (ptrace(PTRACE_CONT, g_program.pid, 0, 0) < 0)
+        return 0;
+
+    handle_wait();
+    return 1;
+}
