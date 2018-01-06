42117dd1151eeec7b2cd843ad887e4d1bfd702e6
Hugo Linsenmaier <hugo.linsenmaier@epita.fr>
2018-01-06 23:46:45 +0100

    [ADD] backtrace.c: Implement backtrace using libunwind

--- src/backtrace.c
+++ src/backtrace.c
@@ -1,4 +1,21 @@
-//
-// Created by hugo on 06/01/18.
-//
+#define UNW_LOCAL_ONLY
+#include <libunwind.h>
+#include <stdio.h>
 
+int backtrace(void *arg)
+{
+    arg = arg;
+    unw_cursor_t cursor;
+    unw_context_t uc;
+    unw_word_t ip;
+    unw_word_t sp;
+
+    unw_getcontext(&uc);
+    unw_init_local(&cursor, &uc);
+    while (unw_step(&cursor) > 0) {
+        unw_get_reg(&cursor, UNW_REG_IP, &ip);
+        unw_get_reg(&cursor, UNW_REG_SP, &sp);
+        printf("ip = 0x%lx, sp = 0x%lx\n", (long) ip, (long) sp);
+    }
+    return 1;
+}
