11a3bf32ea9d34ca945a60465a153929d0b04538
Hugo Linsenmaier <hugo.linsenmaier@epita.fr>
2018-01-07 17:37:42 +0100

    [MODIFY] main.c: Check if file is executable

--- src/main.c
+++ src/main.c
@@ -1,7 +1,15 @@
 #include <err.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 #include "debug.h"
+
+static int is_executable(char *filename)
+{
+    struct stat sb;
+    return stat(filename, &sb) == 0 || sb.st_mode & S_IXUSR;
+}
 
 int main(int argc, char *argv[])
 {
@@ -9,6 +17,9 @@ int main(int argc, char *argv[])
         warnx("Usage: ./my-dbg /path/to/binary arguments...");
         return 1;
     }
+
+    if (!is_executable(argv[1]))
+        return 1;
 
     printf("Program being debugged is %s\n", argv[1]);
     if (!debug(argv))
